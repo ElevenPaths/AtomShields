9c2062aa2591320ac2ff2c45579ace8488452179
Diego Fernandez <diego.fernandez@11paths.com>
2018-03-19 11:26:44 +0100

    Clean code

--- atomshields/reports/base.py
+++ atomshields/reports/base.py
@@ -7,16 +7,34 @@ class GenericReport(object):
 	"""
 
 	def __init__(self, issues):
+		"""
+		Class constructor.
+
+		Args:
+			issues (list): List of `Issue` instances
+		"""
 		self._issues = []
 		self.issues = issues
 
-
 	@property 
 	def issues(self):
+		"""
+		Getter for 'issues' property
+
+		Returns:
+			list: List of `Issue` instances
+		"""
 		return self._issues
 
 	@issues.setter
 	def issues(self, value):
+		"""
+		Setter for 'issues' property
+
+		Args:
+			value (list): List of `Issue` instances
+
+		"""
 		self._issues = value
 
 
