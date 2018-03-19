8a3bf8f9b4f73008bb9815931d540e0e1fcef826
Diego Fernandez <diego.fernandez@11paths.com>
2018-03-19 11:30:24 +0100

    Clean code

--- atomshields/reports/http.py
+++ atomshields/reports/http.py
@@ -1,12 +1,13 @@
 # -*- coding:utf8 -*-
 from reports.base import GenericReport
+import requests
 
 
 class HttpReport(GenericReport):
 	"""
 	This module sends all information about vulnerabilities to an endpoint via an http request.
 
-    Attributes:
+	Attributes:
         NAME (str): Name of the module.
         DESCRIPTION (str): Description of the functionality of the module.
         CONFIG (dict): Default values of the module configuration.
@@ -19,8 +20,16 @@ class HttpReport(GenericReport):
 		"url": "<your_endpoint>",
 		"method": "post"
 	}
+
 	def __init__(self, *args, **kwrds):
+		"""
+		Class constuctor. Must call parent constructor
+		"""
 		super(HttpReport, self).__init__(*args, **kwrds)
 
 	def run(self):
-		print "Enviando por http"
+		"""
+		Method executed dynamically by framework. This method will do a http request to
+		endpoint setted into config file with the issues and other data.
+		"""
+		pass
